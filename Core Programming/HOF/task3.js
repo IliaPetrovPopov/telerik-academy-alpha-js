fbec1932ee5f437cec8872f28a7ce84256fcf7af
Ilia Popov <ilia_popov02@abv.bg>
2023-04-28 07:28:49 +0300

    Arrange files in different folders

---
+++ Core Programming/HOF/task3.js
@@ -0,0 +1,16 @@
+// Powerful pets
+/**
+ * 
+ * @param {Array<{ name: string, pets: Array<{ name: string, power: number }> }>} data 
+ */
+export default function (data) {
+  // your code starts here
+  const result = data.reduce((acc, currObj) => {
+
+    return acc;
+    
+  }, [])
+
+  return result;
+  // your code ends here
+}
