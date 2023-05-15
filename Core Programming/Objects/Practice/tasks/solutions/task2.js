418c8c158430460c5bfa607ba822b53c203c2982
Ilia Popov <ilia_popov02@abv.bg>
2023-05-15 09:44:27 +0300

    Add object practice task-2

---
+++ Core Programming/Objects/Practice/tasks/solutions/task2.js
@@ -0,0 +1,11 @@
+// Game of Key-owns
+
+/**
+ * 
+ * @param {{ keys: string[], first: object, second: object }} data 
+ */
+export default function (data) {
+  // your code starts here
+
+  // your code ends here
+}
