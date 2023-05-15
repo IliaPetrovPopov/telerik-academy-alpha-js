8c48bf6134ccbb0ffe12d6a970b0bbf5bd837a9b
Ilia Popov <ilia_popov02@abv.bg>
2023-05-15 09:44:54 +0300

    Add object practice task-3

---
+++ Core Programming/Objects/Practice/tasks/solutions/task3.js
@@ -0,0 +1,17 @@
+// Graduates
+/**
+ * 
+ * @param {{
+    course: string;
+    minPassingGrade: number;
+    students: {
+        name: string;
+        grades: number[];
+    }[];
+}} data 
+ */
+export default function (data) {
+  // your code starts here
+
+  // your code ends here
+}
