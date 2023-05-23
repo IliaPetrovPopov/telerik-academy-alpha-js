14e6ea91de2a5847c301ccaf3e5f920e1748a14f
Ilia Popov <ilia_popov02@abv.bg>
2023-05-23 12:53:47 +0300

    Add fields, constructor & getters of issue.model

--- Object Oriented Programming/Inheritance & Composition/Template/src/models/board-items/issue.model.js
+++ Object Oriented Programming/Inheritance & Composition/Template/src/models/board-items/issue.model.js
@@ -2,39 +2,54 @@ import { BoardItem } from './board-item.model';
 import { issueStatus } from '../../common/issue-status.enum';
 
 export class Issue extends BoardItem {
+    /** The date when the issue was created. */
     #createdOn;
-    #resolvedOn;
-    #description;
+
+    /** The date then the issue was resolved. */
+    #resolvedOn = null;
+
+    /** Current issue's status. */
     #status;
 
-    constructor(name, createdOn, resolvedOn, descri) {
+    /** Description of current issue. */
+    #description;
+
+    /** Minimum length for description. */
+    static #MIN = 10;
+
+    /** Maximum length for description. */
+    static #MAX = 40;
+
+    /**
+     * Constructor for current issue.
+     * @param {string} name Current issue's name.
+     * @param {string} description Current issue's description.
+     */
+    constructor(name, description) {
         super(name);
-        this.createdOn = createdOn;
-
-    }
-
-    get createdOn() {
-        return 
-    }
-
-    get resolvedOn() {
-
-    }
-
-    reset() {
+        this.description = description;
+        this.#createdOn = new Date();
         this.#status = issueStatus.RAISED;
     }
-
-    advance() {
-        this.#status = issueStatus.IN_REVIEW;
+    
+    /** Shows the value of createdOn. */
+    get createdOn() {
+        return this.#createdOn;
     }
 
-    complete() {
-        this.#status = issueStatus.RESOLVED;
+    /** Shows the value of resolvedOn. */
+    get resolvedOn() {
+        return this.#resolvedOn;
     }
 
-    toString() {
-        return `* Issue *\n` +
-                `Name: ${}` +
+    /** Shows the value of status. */
+    get status() {
+        return this.#status;
     }
+
+    /** Shows the value of description. */
+    get description() {
+        return this.#description;
+    }
+
 }
