82a2126ff561599f9189247f09835aef20cfaa13
Ilia Popov <ilia_popov02@abv.bg>
2023-05-23 12:51:27 +0300

    Add board-item implementation

--- Object Oriented Programming/Inheritance & Composition/Template/src/models/board-items/board-item.model.js
+++ Object Oriented Programming/Inheritance & Composition/Template/src/models/board-items/board-item.model.js
@@ -1,4 +1,37 @@
 
 export class BoardItem {
-    
+    /** Name of board item */
+    #name;
+
+    /** The minimum length of name. */
+    static #MIN_NAME_LENGTH = 6;
+
+    /** The minimum length of name. */
+    static #MAX_NAME_LENGTH = 20;
+
+    /**
+     * Constructor of board item.
+     * @param {string} newName Current name.
+     */
+    constructor(newName) {
+        this.name = newName;
+    }
+
+    /** Getter for name. */
+    get name() {
+        return this.#name;
+    }
+
+    /** Sets name to newName, if newName is valid.*/
+    set name(newName) {
+        if(!newName) {
+            throw new Error('Name is not valid value!');
+        }
+
+        if(newName.length < BoardItem.#MIN_NAME_LENGTH || newName.length > BoardItem.#MAX_NAME_LENGTH ) {
+            throw new Error(`Length of name must be between ${BoardItem.#MIN_NAME_LENGTH} and ${BoardItem.#MAX_NAME_LENGTH} `);
+        }
+
+        this.#name = newName;
+    }
 }
