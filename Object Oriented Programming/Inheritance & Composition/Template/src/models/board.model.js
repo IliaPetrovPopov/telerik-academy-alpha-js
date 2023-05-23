4163e7118d319d58d6c274422b48af1dfd256fd5
Ilia Popov <ilia_popov02@abv.bg>
2023-05-23 12:49:03 +0300

    Add board.model.js documentation

--- Object Oriented Programming/Inheritance & Composition/Template/src/models/board.model.js
+++ Object Oriented Programming/Inheritance & Composition/Template/src/models/board.model.js
@@ -2,22 +2,34 @@ import { Task } from './board-items/task.model.js';
 
 /** The Board class holds items */
 export class Board {
+  /** The list where all the tasks are held */
   #items;
 
+  /** Constructor of board. */
   constructor() {
     this.#items = [];
   }
 
+  /** Shows what is inside the board.
+  * @returns {Array} Array with all the tasks inside. 
+  */
   get items() {
     return this.#items.slice();
   }
 
+  /** Shows what is the length of the current board.
+  * @returns {number} Number of tasks.
+  */
   get count() {
     return this.#items.length;
   }
 
+  /**
+   * Adds new task to board.
+   * @param {object} item Task to be added
+   */
   add(item) {
-    if (!(item instanceof Task)) {
+    if (!(item instanceof BoardItem)) {
       throw new Error('The provided value is not an objected created from the BoardItem class!');
     }
 
@@ -29,9 +41,13 @@ export class Board {
 
     this.#items.push(item);
   }
-  
+
+  /**
+   * Removes task from board.
+   * @param {object} item Task to be removed.
+   */
   remove(item) {
-    if (!(item instanceof Task)) {
+    if (!(item instanceof BoardItem)) {
       throw new Error('The provided value is not an objected created from the BoardItem class!');
     }
     
@@ -44,7 +60,9 @@ export class Board {
     this.#items.splice(itemIndex, 1);
   }
 
-  /** Transforms the board data into a formatted string. */
+  /** Transforms the board data into a formatted string. 
+  * @returns {string} The formatted board.
+  */
   toString() {
     const titles = '---Items Board---\n\nItems:\n\n';
 
