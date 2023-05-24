108a1cb68a832d7cb032db93450cca16f471ecba
Ilia Popov <ilia_popov02@abv.bg>
2023-05-24 09:22:36 +0300

    Add private properties & constructor in note.model

--- Object Oriented Programming/Inheritance & Composition/Template/src/models/board-items/note.model.js
+++ Object Oriented Programming/Inheritance & Composition/Template/src/models/board-items/note.model.js
@@ -1,9 +1,34 @@
 import { BoardItem } from './board-item.model.js';
 
 export class Note extends BoardItem {
-    #status
+    /** Description of note */
+    #description;
 
-    constructor()
+    /** Importance of note */
+    #importance;
+
+    /** Status of note */
+    #status;
+
+    /** The minimim length of description. */
+    static #MIN_DESC_LENGTH = 6;
+
+    /** The maximum length of description. */
+    static #MAX_DESC_LENGTH = 60;
+
+    /**
+     * Note's constructor.
+     * @param {string} name String, which shows the name of note.
+     * @param {string} description String, which shows description of note.
+     * @param {string} importance Readonly enum, showing the importance of note.
+     */
+    constructor(name, description, importance) {
+        super(name);
+        this.description = description;
+        this.#importance = importance;
+        this.#status = noteStatus.CREATED;
+    }
+
 
     reset() {
         this.#status = 
