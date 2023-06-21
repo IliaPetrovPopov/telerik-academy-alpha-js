fbc734b74be308f6be64442ce9ca0a1d42b73a74
Ilia Popov <ilia_popov02@abv.bg>
2023-06-21 21:15:09 +0300

    Add template for additional workshop for OOP

---
+++ Object Oriented Programming/10. Additional Workshop/Template/src/models/contact.js
@@ -0,0 +1,27 @@
+import moment from 'moment';
+
+export class Contact {
+  name;
+  phone;
+  history;
+
+  constructor(name, phone) {
+    // missing implementation
+  }
+
+  setPhone(phone) {
+    // missing implementation
+  }
+
+  call() {
+    // missing implementation
+  }
+
+  message(msg) {
+    // missing implementation
+  }
+
+  viewHistoryLog() {
+    // missing implementation
+  }
+}
---
+++ Object Oriented Programming/10. Additional Workshop/Template/src/models/phonebook.js
@@ -0,0 +1,27 @@
+export class Phonebook {
+  contacts;
+
+  constructor() {
+    // missing implementation
+  }
+
+  addContact(contact) {
+    // missing implementation
+  }
+
+  removeContact(name) {
+    // missing implementation
+  }
+
+  updateContact(name, phone) {
+    // missing implementation
+  }
+
+  listAllContacts() {
+    // missing implementation
+  }
+
+  search(partialName) {
+    // missing implementation
+  }
+}
